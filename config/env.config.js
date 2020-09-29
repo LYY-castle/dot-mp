238140933d3f58507d5f40ab5c237abf9724cb0d
cn.shperry <cn.shperry@gmail.com>
2020-09-29 10:13:36 +0800

    恢复体验版接口地址配置为 dev 环境.

--- config/env.config.js
+++ config/env.config.js
@@ -23,7 +23,7 @@ if (__wxConfig.envVersion === 'trial') {
 		VCONSOLE: true,
 		appid: 'wxd8233d62e992e916',
 		appSecret: '7060d0a558597c51c47abc275adde454',
-		VUE_APP_BASE_URL: 'https://dot.hkjindian.com:18900',
+		VUE_APP_BASE_URL: 'https://dot-dev.hkjindian.com:18703',
 		qsebao: {
 			baseURL: 'https://qcapi.qsebao.com',
 			apiKey: '1qjAllYQPTEsk46RbY0xc000018708',
