9da6becd4d2c78e3977c9c118af29569f4ac5788
cn.shperry <cn.shperry@gmail.com>
2020-11-17 14:48:09 +0800

    修复正式环境接口地址错误.

--- config/env.config.js
+++ config/env.config.js
@@ -39,7 +39,7 @@ if (__wxConfig.envVersion === 'release') {
 		VCONSOLE: false,
 		appid: 'wxd8233d62e992e916',
 		appSecret: '7060d0a558597c51c47abc275adde454',
-		VUE_APP_BASE_URL: 'http://dot.hkjindian.com:18900',
+		VUE_APP_BASE_URL: 'https://dot.hkjindian.com:18900',
 		qsebao: {
 			baseURL: 'https://qcapi.qsebao.com',
 			apiKey: '1qjAllYQPTEsk46RbY0xc000018708',
